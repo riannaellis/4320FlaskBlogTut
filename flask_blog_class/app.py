f5e1e1b1f96d6a616019628869ad871c7bf8e427
Rianna Ellis <riannaellis@test-142152.dhcp.missouri.edu>
2024-11-01 13:35:15 -0500

    Added edit(id) and get_post(post_id) functions.

--- flask_blog_class/app.py
+++ flask_blog_class/app.py
@@ -20,6 +20,15 @@ def get_db_connection():
     #return the connection object
     return conn
 
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute("SELECT * FROM posts WHERE id = ?", (post_id,)).fetchone()
+    conn.close()
+
+    if post is None:
+        abort(404)
+    return post
+
 
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
@@ -61,5 +70,36 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
+#create a route to edit a post. Load page with get or post method
+#pass the post id as a URL parameter
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    #get the post from the database with a select query for the post with that id
+    post = get_post(id)
+
+    #determine if the page was requested with GET or POST
+    #if POST, process the form data. Get the data and validate it. Update the post and redirect to the homepage.
+    if request.method == 'POST':
+        #get the title and content
+        title = request.form['title']
+        content = request.form['content']
+
+        #if no title or content, flash an error message
+        if not title:
+            flash('Title is required.')
+        elif not content:
+            flash('Content is required.')
+        else:
+            conn = get_db_connection()
+            conn.execute("UPDATE posts SET title = ?, content = ? WHERE id = ?", (title, content, id))
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('index'))
+    
+    #if GET then display page
+    return render_template('edit.html', post=post)
+    
 
 app.run()
